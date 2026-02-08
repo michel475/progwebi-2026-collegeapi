0c37077b866e4bfc24e1407e4e4be534d0b91429
michel475 <michaelchacal776@gmail.com>
2026-02-08 11:14:53 -0300

    versao inicial

---
+++ src/main/java/progwebi/apicollege/ApicollegeApplication.java
@@ -0,0 +1,13 @@
+package progwebi.apicollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicollegeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/progwebi/apicollege/ApicollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package progwebi.apicollege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
