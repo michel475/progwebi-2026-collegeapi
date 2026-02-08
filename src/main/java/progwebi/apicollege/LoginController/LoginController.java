6387b34d0a50ac7c4417b6edde33cca1b7afdeaa
michel475 <michaelchacal776@gmail.com>
2026-02-08 17:48:05 -0300

    listando estudante com json

--- src/main/java/progwebi/apicollege/LoginController/LoginController.java
+++ src/main/java/progwebi/apicollege/LoginController/LoginController.java
@@ -1,23 +1,33 @@
 package progwebi.apicollege.LoginController;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import progwebi.apicollege.model.student;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class LoginController {
     @GetMapping(path = ("/tela-de-login/"))
-    public String telaDeLogin() {
-        return ("""
-                <html>
-                    <head></head>
-                    <body>
-                    <h1 font-color=blue>Bem vindo</h1>
-                        <label>Usuario</label>
-                        <input type="text" placeholder="Digite seu nome de usuario">
-                        <label>Senha</label>
-                        <input type="password" placeholder="Digite sua senha">
-                    </body>
-                </html>
-                """);
+    public List<student> telaDeLogin() {
+        student estudante = new student();
+        estudante.setId(1l);
+        estudante.setName("Michel");
+        estudante.setCourse("Sistemas de informacao");
+        estudante.setRegisterNumber("20260101");
+        estudante.setRegisterDate(LocalDate.now());
+        student estudante2 = student.builder()
+                .id(2l)
+                .name("Amanda")
+                .registerNumber("20260202")
+                .course("Sistemas de informacao")
+                .registerDate(LocalDate.now())
+                .build();
+        List<student> lista = Arrays.asList(estudante, estudante2);
+        return lista;
     }
 }
